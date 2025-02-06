08f3f937ee781a9b65136c27cd7f6970c385a891
heyaxenthDev <hyacynth.dev@gmail.com>
2025-02-07 07:18:57 +0800

    sub admin auth

--- sub-admin/authentication.php
+++ sub-admin/authentication.php
@@ -3,7 +3,7 @@ session_start();
 
 // Check if the user is not authenticated
 function checkLogin(){
-    if (!isset($_SESSION['admin_auth']) || $_SESSION['admin_auth'] !== true) {
+    if (!isset($_SESSION['sub_admin_auth']) || $_SESSION['sub_admin_auth'] !== true) {
         // Set session variables for status message
         $_SESSION['status'] = "Denied Access!";
         $_SESSION['status_text'] = "Please Login to Access the Page";
